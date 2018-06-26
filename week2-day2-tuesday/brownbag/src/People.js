8a9b89b94422c0163d02c6e10ade493bf5399c2e
Jarid11 <Marinosj1995@gmail.com>
2018-06-26 09:52:08 -0500

    added mass functionality

--- week2-day2-tuesday/brownbag/src/People.js
+++ week2-day2-tuesday/brownbag/src/People.js
@@ -5,10 +5,12 @@ class People extends Component {
     super();
     this.state = {
       height: 0,
-      addHeight: false
+      mass: 0
     };
     this.decreaseHeight = this.decreaseHeight.bind(this);
     this.increaseHeight = this.increaseHeight.bind(this);
+    this.decreaseMass = this.decreaseMass.bind(this);
+    this.increaseMass = this.increaseMass.bind(this);
   }
 
   decreaseHeight() {
@@ -23,16 +25,35 @@ class People extends Component {
     });
   }
 
+  decreaseMass() {
+    this.setState({
+      mass: this.state.mass - 1
+    });
+  }
+
+  increaseMass() {
+    this.setState({
+      mass: this.state.mass + 1
+    });
+  }
+
   render() {
     const { name, height, mass } = this.props;
-    const { height: stateHeight } = this.state;
+    const { height: stateHeight, mass: stateMass } = this.state;
 
     console.log(this.props);
     console.log(this.state);
     return (
       <div className="person">
         <h2>{name}</h2>
-        <div style={{ display: "flex", margin: "auto", width: "60%" }}>
+        <div
+          style={{
+            display: "flex",
+            margin: "auto",
+            width: "75%",
+            justifyContent: "center"
+          }}
+        >
           <button onClick={this.decreaseHeight}>-</button>
           <h4 style={{ margin: "0 8px" }}>
             height:
@@ -40,12 +61,25 @@ class People extends Component {
           </h4>
           <button onClick={this.increaseHeight}>+</button>
         </div>
-        <h4>mass: {mass}</h4>
+        <div
+          style={{
+            display: "flex",
+            margin: "auto",
+            marginTop: "10px",
+            width: "75%",
+            justifyContent: "center"
+          }}
+        >
+          <button onClick={this.decreaseMass}>-</button>
+          <h4 style={{ margin: "0 8px" }}>
+            mass:
+            {parseInt(mass) + stateMass}
+          </h4>
+          <button onClick={this.increaseMass}>+</button>
+        </div>
       </div>
     );
   }
 }
 
 export default People;
-
-// {addHeight ? parseInt(this.props.height, 10) + 50 : this.props.height}
